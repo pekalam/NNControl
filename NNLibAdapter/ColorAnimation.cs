f7dabcc1d79bd229e163e6b570ed8964b4a88ecd
Marek Pękala <marekbf3@gmail.com>
2020-05-02 14:27:33 +0200

    color per layer

--- NNLibAdapter/ColorAnimation.cs
+++ NNLibAdapter/ColorAnimation.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
+using MathNet.Numerics.LinearAlgebra;
 using NNControl.Network;
 using NNLib;
 
@@ -21,58 +22,16 @@ namespace NNLibAdapter
             _trainer.EpochEnd += TrainerEpochEnd;
         }
 
-        
-        private (double mingz, double maxgz, double minlz, double maxlz) FindMinMaxW(IReadOnlyList<Layer> layers)
+
+        private (double mingz, double maxgz, double minlz, double maxlz) FindMinMax(Layer layer)
         {
             double mingz = 0, maxgz = 0, minlz = 0, maxlz = 0;
-            foreach (var layer in layers)
+
+            for (int i = 0; i < layer.Weights.RowCount; i++)
             {
-                for (int i = 0; i < layer.Weights.RowCount; i++)
+                for (int j = 0; j < layer.Weights.ColumnCount; j++)
                 {
-                    for (int j = 0; j < layer.Weights.ColumnCount; j++)
-                    {
-                        var el = layer.Weights[i, j];
-                        if (el < 0)
-                        {
-                            if (el < minlz)
-                            {
-                                minlz = el;
-                            }
-
-                            if (el > maxlz)
-                            {
-                                maxlz = el;
-                            }
-                        }
-                        else
-                        {
-                            if (el < mingz)
-                            {
-                                mingz = el;
-                            }
-
-                            if (el > maxgz)
-                            {
-                                maxgz = el;
-                            }
-
-                        }
-                        
-                    }
-                }
-            }
-
-            return (mingz, maxgz, minlz, maxlz);
-        }
-
-        private (double mingz, double maxgz, double minlz, double maxlz) FindMinMaxB(IReadOnlyList<Layer> layers)
-        {
-            double mingz = 0, maxgz = 0, minlz = 0, maxlz = 0;
-            foreach (var layer in layers)
-            {
-                for (int i = 0; i < layer.Biases.RowCount; i++)
-                {
-                    var el = layer.Biases[i, 0];
+                    var el = layer.Weights[i, j];
                     if (el < 0)
                     {
                         if (el < minlz)
@@ -96,10 +55,49 @@ namespace NNLibAdapter
                         {
                             maxgz = el;
                         }
-
                     }
                 }
             }
+
+
+
+
+            return (mingz, maxgz, minlz, maxlz);
+        }
+
+        private (double mingz, double maxgz, double minlz, double maxlz) FindMinMaxB(Layer layer)
+        {
+            double mingz = 0, maxgz = 0, minlz = 0, maxlz = 0;
+
+            for (int i = 0; i < layer.Biases.RowCount; i++)
+            {
+                var el = layer.Biases[i, 0];
+                if (el < 0)
+                {
+                    if (el < minlz)
+                    {
+                        minlz = el;
+                    }
+
+                    if (el > maxlz)
+                    {
+                        maxlz = el;
+                    }
+                }
+                else
+                {
+                    if (el < mingz)
+                    {
+                        mingz = el;
+                    }
+
+                    if (el > maxgz)
+                    {
+                        maxgz = el;
+                    }
+                }
+            }
+
 
             return (mingz, maxgz, minlz, maxlz);
         }
@@ -110,6 +108,7 @@ namespace NNLibAdapter
             {
                 return 127;
             }
+
             var s = (int) ((x - minx) * (maxy - miny) / (maxx - minx)) + start;
             //Debug.WriteLine($"{s} for {x} max:{maxx} min: {minx}");
             // if (s >= 126 || s <= 128)
@@ -126,12 +125,12 @@ namespace NNLibAdapter
 
             var layers = _trainer.Network.Layers;
 
-            var (mingz, maxgz, minlz, maxlz) = FindMinMaxW(layers);
-            var (minbgz, maxbgz, minblz, maxblz) = FindMinMaxB(layers);
 
 
             for (int i = 0; i < layers.Count; i++)
             {
+                var (mingz, maxgz, minlz, maxlz) = FindMinMax(layers[i]);
+
                 for (int j = 0; j < layers[i].Weights.RowCount; j++)
                 {
                     for (int k = 0; k < layers[i].Weights.ColumnCount; k++)
@@ -152,6 +151,8 @@ namespace NNLibAdapter
 
             for (int i = 0; i < layers.Count; i++)
             {
+                var (minbgz, maxbgz, minblz, maxblz) = FindMinMaxB(layers[i]);
+
                 for (int j = 0; j < layers[i].Biases.RowCount; j++)
                 {
                     var el = layers[i].Biases[j, 0];
@@ -161,7 +162,7 @@ namespace NNLibAdapter
                     }
                     else
                     {
-                        color.SetNeuronColor(i + 1, j, Scale(el, 0, maxbgz, 0, 127,128));
+                        color.SetNeuronColor(i + 1, j, Scale(el, 0, maxbgz, 0, 127, 128));
                     }
                 }
             }
